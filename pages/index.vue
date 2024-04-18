1ee9b79e00b4f9b5fc47085451203f790d63f640
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2024-04-18 13:13:13 +0000

    feat: API連携用の最小構成を構築

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+
+const id = ref(0);
+const query = ref<string | null>("");
+const inputId = ref(0);
+const inputQuerry = ref("");
+
+const fetchData = async () => {
+  const response = await $fetch<{ item_id: number; q: string | null }>(
+    `items/${inputId.value}`,
+    {
+      method: "GET",
+      query: { q: inputQuerry.value },
+      baseURL: runtimeConfig.public.apiBase,
+    }
+  );
+  id.value = response.item_id;
+  query.value = response.q;
+};
+</script>
+
+<template>
+  <div>
+    <input v-model="inputId" />
+    <input v-model="inputQuerry" />
+    <button @click="fetchData">Fetch ID</button>
+  </div>
+  <div>{{ id }}</div>
+  <div>{{ query }}</div>
+</template>
